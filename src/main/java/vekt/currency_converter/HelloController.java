ee968bfba0f2ec332c29f6e317363d7677c273e9
AbdulS123 <abdul.tuna@stud.fh-campuswien.ac.at>
2024-01-08 11:08:24 +0100

    Renamed files.

--- src/main/java/vekt/currency_converter/HelloController.java
+++ src/main/java/vekt/currency_converter/HelloController.java
@@ -1,11 +1,9 @@
 package vekt.currency_converter;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,10 +11,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HelloController {
+
+    private int pointForEuro = 0;
+    private int pointForDollar = 0;
+    private int pointForBitcoin = 0;
+    private int pointForDogecoin = 0;
+    private final List<String> questions = List.of("Which currency is the most valuable?", "Which currency is the least valuable?", "Which currency is the most popular?", "Which currency is the least popular?");
+    private int questionNumber = -1;
+    private final int numberofquestion = 7;
+
+
 
     public Double exchangeRateGetter(String currencyFrom, String currencyTo) throws IOException {
         // Create a URL object with the endpoint you want to request
@@ -39,9 +48,9 @@ public class HelloController {
         reader.close();
         return exchangeRateValueExtractor(response.toString());
     }
-
     public Double exchangeRateValueExtractor(String exchangeRateValue){
-        String regex = "<div class=\"YMlKec fxKbKc\">([0-9]+(?:,)?[0-9]+\\.[0-9]+)</div>";
+        //(?:,)?
+        String regex = "<div class=\"YMlKec fxKbKc\">(\\d+,?\\d?\\d?\\d?,?\\d?\\d?\\d?\\.\\d+)</div>";
 
         Matcher matcher = Pattern.compile(regex).matcher(exchangeRateValue);
 
@@ -51,22 +60,15 @@ public class HelloController {
         return stringCleaner(extractedNumber);
 
     }
-
-public Double stringCleaner(String extractedNumber){
-    // Remove the comma from the string
-    String cleanedNumberString = extractedNumber.replace(",", "");
-    // Parse the string into a Double
-    return Double.parseDouble(cleanedNumberString);
-}
-
-
-
-
-
-
-
+    public Double stringCleaner(String extractedNumber){
+        String cleanedNumberString = extractedNumber.replace(",", "");
+        return Double.parseDouble(cleanedNumberString);
+    }
     @FXML
     void btn_convert_click(MouseEvent event) throws IOException {
+        if(tf_amount.getText().isEmpty()){
+            return;
+        }
         Double currencyRate = exchangeRateGetter((String) cbx_from.getValue(), (String) cbx_to.getValue());
         lb_total_currency_value.setText(String.format("%,.6f", Double.parseDouble(tf_amount.getText()) * currencyRate));
 
@@ -74,17 +76,124 @@ public Double stringCleaner(String extractedNumber){
         lb_single_from_currency.setText("1 "+(String) cbx_from.getValue()+" =");
         lb_single_to_currency_name.setText((String) cbx_to.getValue());
         lb_single_to_currency.setText(String.format("%,.6f", currencyRate));
-
-
     }
+
+
+
+
+
+
+
 
     @FXML
     void btn_start_quiz_click(MouseEvent event) {
-
-
-
-
+        grid_main_page.setVisible(false);
+        grid_questions_page.setVisible(true);
+        resetQuiz();
+        System.out.println(lb_amount.getFont());
+        System.out.println(lb_amount.getBackground());
     }
+
+    public void nextQuestion() {
+        if(questionNumber == numberofquestion){
+            grid_questions.setVisible(false);
+            grid_questions_result.setVisible(true);
+            return;
+        }
+        pb_quiz.setProgress((double)(1/numberofquestion)*questionNumber);
+        reset_answer_btn_color();
+        questionChecker();
+        _quiz.setText(questions.get(questionNumber++));
+    }
+    public void questionChecker() {
+        if(btn_first_answer.getStyle().contains("#29bc1e")){
+            pointForEuro++;
+        } else if (btn_second_answer.getStyle().contains("#29bc1e")){
+            pointForDollar++;
+        } else if (btn_third_answer.getStyle().contains("#29bc1e")){
+            pointForBitcoin++;
+        } else if (btn_fourth_answer.getStyle().contains("#29bc1e")){
+            pointForDogecoin++;
+        }
+    }
+
+    @FXML
+    void btn_second_answer_click(MouseEvent event) {
+        if(btn_second_answer.getStyle().contains("#e9e9e9")){
+            reset_answer_btn_color();
+            btn_second_answer.setStyle("-fx-background-color: #29bc1e; -fx-text-fill: #fff; -fx-font-bold: true; -fx-font-size: 16px; -fx-border-radius: 10px; -fx-background-radius: 10px; -fx-border-color: #29bc1e; -fx-border-width: 2px;");
+        }
+        else{ reset_answer_btn_color();}
+    }
+    @FXML
+    void btn_third_answer_click(MouseEvent event) {
+        if(btn_third_answer.getStyle().contains("#e9e9e9")){
+            reset_answer_btn_color();
+            btn_third_answer.setStyle("-fx-background-color: #29bc1e; -fx-text-fill: #fff; -fx-font-bold: true; -fx-font-size: 16px; -fx-border-radius: 10px; -fx-background-radius: 10px; -fx-border-color: #29bc1e; -fx-border-width: 2px;");
+        }
+        else{ reset_answer_btn_color();}
+    }
+    @FXML
+    void btn_fourth_answer_click(MouseEvent event) {
+        if(btn_fourth_answer.getStyle().contains("#e9e9e9")){
+            reset_answer_btn_color();
+            btn_fourth_answer.setStyle("-fx-background-color: #29bc1e; -fx-text-fill: #fff; -fx-font-bold: true; -fx-font-size: 16px; -fx-border-radius: 10px; -fx-background-radius: 10px; -fx-border-color: #29bc1e; -fx-border-width: 2px;");
+        }
+        else{ reset_answer_btn_color();}
+    }
+    @FXML
+    void btn_first_answer_click(MouseEvent event) {
+        if(btn_first_answer.getStyle().contains("#e9e9e9")){
+            reset_answer_btn_color();
+            btn_first_answer.setStyle("-fx-background-color: #29bc1e; -fx-text-fill: #fff; -fx-font-bold: true; -fx-font-size: 16px; -fx-border-radius: 10px; -fx-background-radius: 10px; -fx-border-color: #29bc1e; -fx-border-width: 2px;");
+        }
+        else{ reset_answer_btn_color();}
+    }
+
+    public void reset_answer_btn_color() {
+        btn_first_answer.setStyle("-fx-background-color: #e9e9e9; -fx-text-fill: #444951; -fx-font-bold: true; -fx-font-size: 16px; -fx-border-radius: 10px; -fx-background-radius: 10px;");
+        btn_second_answer.setStyle("-fx-background-color: #e9e9e9; -fx-text-fill: #444951; -fx-font-bold: true; -fx-font-size: 16px;-fx-border-radius: 10px; -fx-background-radius: 10px;");
+        btn_third_answer.setStyle("-fx-background-color: #e9e9e9; -fx-text-fill: #444951; -fx-font-bold: true; -fx-font-size: 16px;-fx-border-radius: 10px; -fx-background-radius: 10px;");
+        btn_fourth_answer.setStyle("-fx-background-color: #e9e9e9; -fx-text-fill: #444951; -fx-font-bold: true; -fx-font-size: 16px;-fx-border-radius: 10px; -fx-background-radius: 10px;");
+    }
+    public void resetQuiz() {
+        pointForEuro = 0;
+        pointForDollar = 0;
+        pointForBitcoin = 0;
+        pointForDogecoin = 0;
+        questionNumber = -1;
+        reset_answer_btn_color();
+    }
+    public void goBackMainPage() {
+        grid_main_page.setVisible(true);
+        grid_questions_page.setVisible(false);
+    }
+
+
+
+
+
+
+    @FXML
+    private Label _quiz;
+    @FXML
+    private GridPane grid_questions;
+    @FXML
+    private GridPane grid_main_page;;
+    @FXML
+    private GridPane grid_questions_page;
+    @FXML
+    private GridPane grid_questions_result;
+    @FXML
+    private Button btn_first_answer;
+    @FXML
+    private Button btn_second_answer;
+    @FXML
+    private Button btn_third_answer;
+    @FXML
+    private Button btn_fourth_answer;
+    @FXML
+    private ProgressBar pb_quiz;
 
 
 
@@ -124,4 +233,108 @@ public Double stringCleaner(String extractedNumber){
         private ComboBox<?> cbx_from;
         @FXML
         private ComboBox<?> cbx_to;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        @FXML
+        private GridPane grid_underground;
 }
